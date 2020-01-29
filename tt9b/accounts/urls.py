ebdbbdd0db45d4b9670af3455acc521851801a18
Josiah M <jcm5155@gmail.com>
2020-01-28 21:13:47 -0600

    minor comment updates

--- tt9b/accounts/urls.py
+++ tt9b/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
-
+# Url patterns for auth API
 urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/auth/register', RegisterAPI.as_view()),
