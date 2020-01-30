fb8517749165148e9186bab80fe3de95877c7d9c
Josiah M <jcm5155@gmail.com>
2020-01-30 10:04:01 -0600

    its gotta work this time

--- tt9b/settings.py
+++ tt9b/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tt9b.wsgi.application'
 
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
