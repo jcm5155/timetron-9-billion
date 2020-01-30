8e43fc21fc838df45c77d1c8d9edcda6fb1467c0
Josiah M <jcm5155@gmail.com>
2020-01-30 02:29:36 -0600

    ,,,,,,,

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-export const entry = "./tt9b/frontend/src/index.js";
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
-    },
-    {
-      test: /\.css$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader",
-          options: {
-            sourceMap: true
-          }
+module.exports = {
+  entry: "./tt9b/frontend/src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
-  ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
